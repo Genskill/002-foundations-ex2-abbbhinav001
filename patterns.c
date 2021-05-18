79f443c985b14c6ff04ace3077b33d9ef97301da
Abhinav <47878378+abbbhinav001@users.noreply.github.com>
2021-05-18 15:40:45 +0530

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main()
+{
+	int n, r, i, j, k;
+	n = get_int("Enter type of pattern (1 or 2) ");
+  r = get_int("Number of rows ");
+	switch(n)
+	{
+		case 1 : {
+			for(i=0;i<r;i++)
+			{
+				for(j=0;j<=i;j++)
+					printf("#");
+				printf("\n");
+			}
+			break;
+		}
+		
+		case 2 : {
+			for(i=r;i>0;i--)
+			{
+				for(j=0;j<i-1;j++)
+					printf(" ");
+				for(k=0;k<(r-j);k++)
+					printf("#");
+				printf("\n");
+			}
+			break;
+		}
+		default : printf("Read carefully!!!");
+	}
+}
